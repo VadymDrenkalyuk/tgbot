e8156c24cf0363bdda8fd17f69f63802f833b347
VadymDrenkalyuk <39189803+VadymDrenkalyuk@users.noreply.github.com>
2025-03-09 23:17:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram.ext import Application, MessageHandler, CommandHandler, filters
+from telegram.ext import CallbackContext
 
 # Твой токен бота от BotFather
 TOKEN = "7925748933:AAEW70GhZ_ubQhiiRDbY3LGWss0-Vun17-E"
@@ -8,20 +9,29 @@ TOKEN = "7925748933:AAEW70GhZ_ubQhiiRDbY3LGWss0-Vun17-E"
 CHANNEL_ID = "@dnipro_alerts"
 
 # Ключевые слова для фильтрации
-KEYWORDS = ["Дніпро", "Дніпра", "Ігрень" "Ігрені" "Павлоград" "Павлограду" "Павлограда"]  # Можно добавить свои
+KEYWORDS = ["дніпро", "дніпра", "ігрені", "павлоград", "павлограду", "павлограда"]  # Убедись, что слова в нижнем регистре
 
 # Создание бота
 app = Application.builder().token(TOKEN).build()
 
-async def filter_messages(update: Update, context):
+# Обработчик команды /start
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text("Dnipro Alerts will notify you about "дніпро", "дніпра", "ігрені", "павлоград", "павлограду", "павлограда" only")
+
+# Фильтрация сообщений
+async def filter_messages(update: Update, context: CallbackContext):
     """Обрабатывает новые сообщения и пересылает только нужные"""
     text = update.message.text.lower()
     
     if any(keyword in text for keyword in KEYWORDS):
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f"📢 Фильтр: {update.message.text}")
 
-# Добавляем обработчик сообщений
+# Добавляем обработчик команды /start
+app.add_handler(CommandHandler("start", start))
+
+# Добавляем обработчик фильтрации сообщений
 app.add_handler(MessageHandler(filters.TEXT & filters.Chat(CHANNEL_ID), filter_messages))
 
-print("Бот запущен!")
+# Запуск бота
+print("Bot started!")
 app.run_polling()
